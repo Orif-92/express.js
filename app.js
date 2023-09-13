ff2eda1cbf03df29b5d3a966ddd6e18aadda33cf
Orif-92 <sarniyozovorif@gmail.com>
2023-09-13 07:55:30 +0500

    //GET - /books/:id:

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get('/books', (req, res) => {
   res.json(books);
 });
 
+//GET - /books/:id:
+app.get('/books/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const book = books.find((item) => item.id === id);
+
+  if (!book) {
+    res.status(404).json({ message: 'Ma\'lumot topilmadi' });
+  } else {
+    res.json(book);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server ${PORT}-portda ishga tushdi`);
 });
